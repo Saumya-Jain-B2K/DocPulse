68dba79439e438dfbad41723a53de303280a3043
Saumya-Jain-B2K <saumyajain2020@gmail.com>
2025-09-20 13:46:31 +0530

    completed appointment page and telatd doctors page

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -6,10 +6,13 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
 
+    const currencySymbol = "₹";
+
 
 
     const value = {
-        doctors
+        doctors, 
+        currencySymbol
     }
 
     return (
