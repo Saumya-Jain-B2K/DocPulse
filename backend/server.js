ba375f0592b628358df08151c619427038805faf
Saumya-Jain-B2K <saumyajain2020@gmail.com>
2025-09-30 23:17:29 +0530

    worked on admincontroller

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import connectDB from './config/mongodb.js';
 import 'dotenv/config';
 import connectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js';
 
 
 // app config
@@ -16,7 +17,8 @@ app.use(express.json());
 app.use(cors()); 
 
 //api endpoints
-
+app.use('/api/admin', adminRouter);
+// localhost:4000/api/admin/add-doctor
 
 app.get('/', (req, res) => {
     res.send("API is working")
