97987936f55a18d79fb93487b5d633dbd55c327f
Saumya-Jain-B2K <saumyajain2020@gmail.com>
2025-10-07 23:36:33 +0530

    created the proper api to show all the appointments in my appointments page

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -172,4 +172,19 @@ const bookAppointment = async (req, res) => {
     }
 }
 
-export {registerUser, loginUser, getProfile, updateProfile, bookAppointment}
+// api to get user appointments for frontend my-appointment page
+
+const listAppointment = async (req, res) => {
+    try {
+        const userId = req.userId
+        const appointments = await appointmentModel.find({userId})
+
+        res.json({success: true, appointments})
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
+}
+
+
+export {registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment}
