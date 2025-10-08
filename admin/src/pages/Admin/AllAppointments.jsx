9dc3d8c74f72f0454fec20552e299d9a1cee6882
riyaharchandani <riyaharchandani76@gmail.com>
2025-10-08 20:04:24 +0530

    creating UI of admin

--- admin/src/pages/Admin/AllAppointments.jsx
+++ admin/src/pages/Admin/AllAppointments.jsx
@@ -12,8 +12,28 @@ const AllAppointments = () => {
     }
   }, [aToken])
   return (
-    <div>
-      AllAppointments
+    <div className='w-full max-w-6xl m-5'>
+
+      <p className='mb-3 text-lg font-medium'>All Appointments</p>
+
+      <div className='bg-white border rounded text-sm max-h-[80vh] min-h-[60vh] overflow-y-scroll'>
+
+        <div className='hideden sm:grid grid-cols-[0.5fr_3fr_1fr_3fr_3fr_1fr_1fr] grid-flow-col py-3 px-6 border-b'>
+          <p>#</p>
+          <p>Patient</p>
+          <p>Age</p>
+          <p>Date & Time</p>
+          <p>Doctor</p>
+          <p>Fees</p>
+          <p>Actions</p>
+        </div>
+
+        {appointments.map((item,index)=>(
+          <div key={index}>
+            <p>{index+1}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
